59b430e4aa25c1298bc02b264165cd53a96d7bb7
dahailu <ludahai19@163.com>
2020-08-21 18:28:37 +0800

    add inference_system

---
+++ tools/infer/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+================================================================
+Copyright (C) 2019 * Ltd. All rights reserved.
+PROJECT      :  PaddleOCR
+FILE_NAME    :  __init__.py
+AUTHOR       :  DAHAI LU
+TIME         :  2020/8/18 下午3:57
+PRODUCT_NAME :  PyCharm
+================================================================
+"""
